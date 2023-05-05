676140d61e804ded50b1a92db3ef59059c86e387
jaimediazcam <1755120@alu.murciaeduca.es>
2023-05-05 13:56:29 +0200

    Ejercicio3 JuezLTI

--- src/clasepersona2/ClasePersona2.java
+++ src/clasepersona2/ClasePersona2.java
@@ -18,7 +18,7 @@ public class ClasePersona2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        System.out.println("Tercer Commit");
+        System.out.println("Cuarto Commit");
     }
     
 }
